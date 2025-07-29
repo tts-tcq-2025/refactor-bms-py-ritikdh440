fa156ab2fe2b736ed5e25e897893f30d27fcf1eb
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2025-07-29 10:47:04 +0530

    refactored_Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,43 @@
-
 from time import sleep
 import sys
 
+def is_temperature_ok(temp):
+    return 95 <= temp <= 102
+
+def is_pulse_ok(pulse):
+    return 60 <= pulse <= 100
+
+def is_spo2_ok(spo2):
+    return spo2 >= 90
+
+def vitals_check(temperature, pulseRate, spo2):
+    return {
+        "temperature": is_temperature_ok(temperature),
+        "pulse": is_pulse_ok(pulseRate),
+        "spo2": is_spo2_ok(spo2)
+    }
+
+def blink_alert(duration=6):
+    for _ in range(duration):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+
+def alert_if_critical(vitals_status):
+    if not vitals_status["temperature"]:
+        print("Temperature critical!")
+        blink_alert()
+    if not vitals_status["pulse"]:
+        print("Pulse Rate is out of range!")
+        blink_alert()
+    if not vitals_status["spo2"]:
+        print("Oxygen Saturation out of range!")
+        blink_alert()
+    return all(vitals_status.values())
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    vitals_status = vitals_check(temperature, pulseRate, spo2)
+    return alert_if_critical(vitals_status)

6cd30fe1f369642e13c0f3d9dcb1adc6d64ae787
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2025-08-05 10:52:02 +0530

    Create printer.py

---
+++ printer.py
@@ -0,0 +1,8 @@
+def default_printer(message):
+    """
+    Default printer function. Prints to console.
+    
+    Args:
+        message (str): The message to be printed.
+    """
+    print(message)
